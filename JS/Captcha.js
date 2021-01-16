88f4e47b51bb60ca8e8cf874837f574fe41dc260
Kirill Korolevsky <i@korolevsky.me>
2021-01-16 19:16:20 +0300

    Refactoring code

--- JS/Captcha.js
+++ JS/Captcha.js
@@ -9,13 +9,12 @@ include('https://www.google.com/recaptcha/api.js?render=6LeF8NwUAAAAAN52GqRPC-5G
 window.onload = function () {
     grecaptcha.ready(function() {
         grecaptcha.execute('6LeF8NwUAAAAAN52GqRPC-5GfuwnCAvuupTDSO9y', {action: 'homepage'}).then(function(token) {
-            let location2;
-            const url = new URL(window.location.href);
+            const params = new URL(window.location.href).searchParams;
 
-            if(url.searchParams.get('redirect_uri') == null) {
-                window.location.href = 'https://api.malex-store.ru/captcha?sid=' + url.searchParams.get('sid') + "&token=" + token;
+            if(params.get('redirect_uri') == null) {
+                window.location.href = 'https://api.malex-store.ru/captcha?sid=' + params.get('sid') + "&token=" + token;
             } else {
-                window.location.href = 'https://api.malex-store.ru/captcha?sid=' + url.searchParams.get('sid') + "&redirect_uri=" + url.searchParams.get('redirect_uri') + "&token=" + token;
+                window.location.href = 'https://api.malex-store.ru/captcha?sid=' + params.get('sid') + "&redirect_uri=" + params.get('redirect_uri') + "&token=" + token;
             }
         });
     });
